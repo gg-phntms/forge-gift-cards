ae2e913d8e2243f86dcdcfb2d01692de177ba381
George <george.gloyens@phntms.com>
2023-10-05 15:01:17 +0100

    Initial commit

---
+++ src/app/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from "@phntms/css-components";
+
+import css from "./page.module.scss";
+
+export const PageContainer = styled("main", {
+  css: css.PageContainer,
+});
---
+++ src/app/styles/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
+
+export const INTER = Inter({
+  variable: "--font-inter",
+  weight: ["400", "700", "900"],
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
+});
+
+export const JB_MONO = JetBrains_Mono({
+  variable: "--font-jb-mono",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  fallback: ["mono"],
+});
+
+export const fontClasses = [INTER.variable, JB_MONO.variable].join(" ");
